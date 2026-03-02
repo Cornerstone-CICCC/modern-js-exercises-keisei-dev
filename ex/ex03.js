22ce010e2ad8b061a6e2afd68ce989570958d6a0
Keisei <imjoshuaa0019@icloud.com>
2026-03-01 19:41:45 -0800

    Refactor numberOfVowels function for clarity

--- ex/ex03.js
+++ ex/ex03.js
@@ -7,21 +7,18 @@ Create a function named numberOfVowels that will receive a string and return the
 
 */
 const numberOfVowels = function (data) {
-  // 1. 数えるための箱を用意
+
   let count = 0;
   
-  // 2. 母音をまとめた文字列を用意
   const vowels = "aeiou";
 
-  // 3. 文字列を1文字ずつチェックする（for...of ループ）
   for (let char of data) {
-    // 4. もし、その文字(char)が vowels の中に含まれていたら
+    
     if (vowels.includes(char)) {
-      count++; // 箱に 1 を足す
+      count++; 
     }
   }
 
-  // 5. 最後に合計を返す
   return count;
 };
 
