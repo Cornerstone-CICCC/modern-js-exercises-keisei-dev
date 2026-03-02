70caf5cab92e5831a9f31f3337e27dcd72ba0096
Keisei <imjoshuaa0019@icloud.com>
2026-03-01 19:40:53 -0800

    Clean up comments in conditionalSum function
    Remove comments for clarity in conditionalSum function.

--- ex/ex02.js
+++ ex/ex02.js
@@ -9,10 +9,7 @@ Create a function named conditionalSum that will be given an array of numbers an
 const conditionalSum = (values, condition) => {
   let sum = 0;
 
-  // 配列を一つずつ取り出す
   for (let num of values) {
-    // 偶数の判定: num % 2 === 0
-    // 奇数の判定: num % 2 !== 0
     
     if (condition === "even" && num % 2 === 0) {
       sum += num;
