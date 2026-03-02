d208bfcfe9a46fa53111d84d7dffed3a7b6e9563
Keisei <imjoshuaa0019@icloud.com>
2026-03-01 19:51:06 -0800

    Clean up comments in ex15.js
    Removed commented Japanese instructions from the code.

--- ex/ex15.js
+++ ex/ex15.js
@@ -15,13 +15,10 @@ const organizeInstructors = function (instructors) {
     const course = instructor.course;
     const name = instructor.name;
 
-    // もし結果オブジェクトにまだそのコース名が存在しない場合、
-    // 空の配列を作成して初期化する
     if (!result[course]) {
       result[course] = [];
     }
 
-    // 対応するコースの配列に講師名を追加する
     result[course].push(name);
   }
 
