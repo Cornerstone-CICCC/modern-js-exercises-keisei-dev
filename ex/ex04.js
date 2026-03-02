3f1a031eaea43d6fd413d7c849f30b0c47ebdc99
Keisei <imjoshuaa0019@icloud.com>
2026-03-01 19:42:42 -0800

    Refactor instructorWithLongestName function comments

--- ex/ex04.js
+++ ex/ex04.js
@@ -10,20 +10,17 @@ Create a function named instructorWithLongestName that will receive an array of 
 */
 
 const instructorWithLongestName = function (instructors) {
-  // 1. 最初の人を暫定チャンピオンにする
+  
   let longest = instructors[0];
 
-  // 2. 1人ずつ順番に見ていく
   for (let person of instructors) {
-    // 3. もし「今の人の名前」の長さが「チャンピオンの名前」より長ければ
-    // .length は文字の長さを教えてくれます
+    
     if (person.name.length > longest.name.length) {
-      // 4. チャンピオン交代！
+      
       longest = person;
     }
   }
 
-  // 5. 最後に残った一番長い名前の人を返す
   return longest;
 };
 
